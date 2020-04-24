8d8092ad1d22312e24d38c64fffc28011a766be9
vo0a <vo0a195@gmail.com>
2020-04-24 17:24:50 +0900

    4장 완료

--- src/main/java/com/mashup/springboot/service/posts/PostsService.java
+++ src/main/java/com/mashup/springboot/service/posts/PostsService.java
@@ -9,7 +9,7 @@ import com.mashup.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,6 +33,15 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
@@ -40,7 +49,7 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
